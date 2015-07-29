50c327002960954635d3fb80bd4c3c183087233f
Vadim Mazur <mazurva@gmail.com>
2015-07-30 01:17:23 +0600

    Update virtual machine for support codeception.

--- tests/codeception/config/config.php
+++ tests/codeception/config/config.php
@@ -14,7 +14,7 @@ return [
     ],
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2_advanced_tests',
+            'dsn' => 'mysql:host=localhost;dbname=yii2_enterprise_tests',
         ],
         'mailer' => [
             'useFileTransport' => true,
