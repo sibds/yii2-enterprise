e581e60d455818f1f03eda5a44289cb67a55ae0d
Vadim Mazur <mazurva@gmail.com>
2015-08-05 11:23:28 +0600

    Fix migrate error on hhvm

---
+++ console/migrations/m130524_201442_init.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m130524_201442_init extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
+}
