43a9a909fe7126b1432aa44e106d368f854b1983
Vadim Mazur <mazurva@gmail.com>
2016-02-20 18:39:32 +0600

    Update

--- backend/themes/adminlte/layouts/main.php
+++ backend/themes/adminlte/layouts/main.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $content string */
 
 
-if (Yii::$app->controller->action->id === 'login') { 
+if (in_array(Yii::$app->controller->action->id, ['login', 'register', 'resend'])) {
 /**
  * Do not use this code in your template. Remove it. 
  * Instead, use the code  $this->layout = '//main-login'; in your controller.
